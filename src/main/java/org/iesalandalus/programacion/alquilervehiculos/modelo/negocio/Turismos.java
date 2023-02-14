ae61ccdb1ed94ad9bb4967fb30ce7b5277398e36
José <Jos@DESKTOP-61Q5ISJ>
2023-02-14 13:31:29 +0100

    Clase Turismos creada.

---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Turismos.java
@@ -0,0 +1,64 @@
+package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.*;
+
+public class Turismos {
+	
+	
+	//DECLARACION
+	
+	public Set<Turismo> coleccionTurismos;
+	
+	
+	//CONSTRUCTORES
+	
+	public Turismos() {
+		coleccionTurismos=new LinkedHashSet<>();
+	}
+	
+	
+	//METODOS DE CLASE
+	
+	public Set<Turismo> get(){
+		Set<Turismo> copiaTurismos=new LinkedHashSet<>(coleccionTurismos.size());
+		for(Turismo i:coleccionTurismos) {
+			copiaTurismos.add(i);
+		}
+		return copiaTurismos;
+	}
+	
+	public int getCantidad() {
+		return coleccionTurismos.size();
+	}
+
+	public void insertar(Turismo turismo) {
+		if(turismo==null) {
+			throw new NullPointerException("ERROR:No puedes insertar un turismo nulo");
+		}
+		if(coleccionTurismos.add(turismo)==false) {
+			throw new IllegalArgumentException("ERROR:El turismo que desea insertar ya existe.");
+		}
+		coleccionTurismos.add(turismo);
+	}
+	public Turismo buscar(Turismo turismo) {
+		if(turismo==null) {
+			throw new NullPointerException("ERROR:No puede buscar un turismo nulo.");
+		}
+		for(Turismo i:coleccionTurismos) {
+			turismo.equals(i);
+			return turismo;
+		}
+		return null;
+	}
+	public void borrar(Turismo turismo) {
+		if(turismo==null) {
+			throw new NullPointerException("ERROR:No puede borrar un turismo nulo.");
+		}
+		if(coleccionTurismos.remove(turismo)==false) {
+			throw new IllegalArgumentException("ERROR:El turismo que desea borrar no existe.");
+		}
+		coleccionTurismos.remove(turismo);
+	}
+}
